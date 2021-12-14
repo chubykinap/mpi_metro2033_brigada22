f90a3bd049aef4db1106ce0f169ef052313c5b39
Herman Gebel <gebel.german@yahoo.com>
2021-12-14 21:35:16 +0300

    test 1: armycontroller.index

---
+++ src/test/java/b22/metro2033/Entity/Army/SoldierTest.java
@@ -0,0 +1,56 @@
+package b22.metro2033.Entity.Army;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoldierTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getRank() {
+    }
+
+    @Test
+    void setRank() {
+    }
+
+    @Test
+    void getHealth_state() {
+    }
+
+    @Test
+    void setHealth_state() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void setUser() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void setPost() {
+    }
+
+    @Test
+    void getCharacteristics() {
+    }
+
+    @Test
+    void setCharacteristics() {
+    }
+}
